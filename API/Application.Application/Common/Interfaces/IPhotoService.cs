65d8f74dd738867b3f38f0521c2a3b6ab5c1545c
Ho Nhat Hy <hy.ho@orientsoftware.com>
2022-08-16 17:45:59 +0700

    Ho Nhat Hy handsome

---
+++ API/Application.Application/Common/Interfaces/IPhotoService.cs
@@ -0,0 +1,11 @@
+﻿using CloudinaryDotNet.Actions;
+
+namespace API.Application.Application.Common.Interfaces
+{
+    public interface IPhotoService
+    {
+        public Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+
+        public Task<DeletionResult> DeletePhotoAsync(string publicId);
+    }
+}
